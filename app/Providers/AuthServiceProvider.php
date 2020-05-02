01100029e918ddfff2084d29b3bc73ec5578634e
Bhushan <bhushangaykawad@gmail.com>
2020-05-02 21:30:50 +0530

    wip

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,24 +7,28 @@ use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+	/**
+	 * The policy mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $policies = [
+		// 'App\Model' => 'App\Policies\ModelPolicy',
+	];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+	/**
+	 * Register any authentication / authorization services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->registerPolicies();
 
-        //
-    }
+		Gate::before(function ($user, $ability) {
+			if ($user->abilities()->contains($ability)) {
+				return true;
+			}
+		});
+	}
 }
